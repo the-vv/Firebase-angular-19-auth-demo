1c9f9423732600c48a67f85932d7cc3bc01acdc5
Vishnu Vinod <vishnuvinod2772001@gmail.com>
2024-12-06 00:45:46 +0530

    login impl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,75 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Auth, GoogleAuthProvider, OAuthProvider, signInWithPopup } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [JsonPipe],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
+    <h1>Angular Firebase Auth Demo</h1>
+    <button (click)="googleSignIn()">Sign in with Google</button>
+    <button (click)="microsoftSignIn()">Sign in with Microsoft</button>
+    <button (click)="signOut()">Sign out</button>
+    <hr>
+    <h3>User Details</h3>
+    <pre style="white-space: pre-wrap;">{{ userJson | json }}</pre>
+    @if (error) {
+      <h3>Error</h3>
+      <pre style="white-space: pre-wrap;">{{ error | json }}</pre>
+    }
   `,
   styles: [],
 })
 export class AppComponent {
-  title = 'firebase-auth-demo';
+  userJson?: {
+    displayName?: string | null,
+    email?: string | null,
+    photoURL?: string | null,
+    uid?: string | null,
+    token?: string | null,
+  } | null;
+
+  error?: any;
+
+  constructor(
+    private _auth: Auth,
+  ) {}
+
+  signOut() {
+    this._auth.signOut();
+    this.userJson = null;
+    this.error = null;
+  }
+
+  ngOnInit() {
+    this._auth.onAuthStateChanged(async user => {
+      console.log('User:', user);
+      this.userJson = user ? {
+        displayName: user.displayName,
+        email: user.email,
+        photoURL: user.photoURL,
+        uid: user.uid,
+        token: await user.getIdToken(),
+      } : null;
+    });
+  }
+
+  googleSignIn() {
+    this.signOut();
+    signInWithPopup(this._auth, new GoogleAuthProvider());
+  }
+
+  microsoftSignIn() {
+    this.signOut();
+    const provider = new OAuthProvider('microsoft.com');
+    signInWithPopup(this._auth, provider)
+      .then((result) => {
+        console.log('Microsoft sign in result:', result);
+      })
+      .catch((error) => {
+        // Handle error.
+        console.error('Microsoft sign in error:', error);
+        this.error = error;
+      });
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,22 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp({
+      apiKey: "AIzaSyDSnzUteB7NVoY65I2CJNzktqmrnzfgIM8",
+      authDomain: "esignature-validation.firebaseapp.com",
+      projectId: "esignature-validation",
+      storageBucket: "esignature-validation.firebasestorage.app",
+      messagingSenderId: "430089803523",
+      appId: "1:430089803523:web:577d0f15d8e8dfd9ad3b36",
+      measurementId: "G-1RBEX8EN84"
+    })),
+    provideAuth(() => getAuth())
+  ]
 };
